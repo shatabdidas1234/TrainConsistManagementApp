f478d16c660e0e8d337f54e5251f16e5153bcf20
shatabdidas1234 <shatabdidas2006@gmail.com>
2026-04-10 14:57:54 +0530

    [RA237] Added UC1

---
+++ App/src/TrainConsistManagementApp.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+    public class TrainConsistManagementApp {
+
+        public static void main(String[] args) {
+            System.out.println("=== Train Consist Management App ===");
+            List<String> trainConsist = new ArrayList<>();
+            System.out.println("Train consist initialized.");
+            System.out.println("Initial bogie count: " + trainConsist.size());
+            System.out.println("Current Train Consist : " + trainConsist);
+            System.out.println("\n System ready for operations...");
+
+        }
+    }
+
